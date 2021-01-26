67df82dbab3d187bdb9c896db9f41d034a8314e3
spenjo7 <palm.pixi.plus@gmail.com>
2021-01-26 00:49:01 -0500

    Refactor to make the results cleaner and easier to manage. Now the returned arrays can include files, folders, or errors instead of generic Items

--- AsyncFileScanRecursive.js
+++ AsyncFileScanRecursive.js
@@ -1,62 +1,83 @@
 /*
-V2.0 2020-05-16: Now Asyncronous ( previously used Sync ). Also includes file/folder details
+V2.0 2020-05-16: 
+	Now Asyncronous ( previously used Sync ). 
+	Now includes file/folder details
+	Now includes bypassing specific folder paths
+
+V2.1 2020-05-17: 
+	Using big arrow syntax on async functions
+	Also cleaned the code up a bit
+	
+V2.2 2020-07-29: 
+	Have an issue where sub-folders show up as arrays with objects in them and I need everything to be flat
+	-- .flat(Infinity) seems to have resolved that issue
+	
+V2.3 2021-01-10: I previously didn't have handling for non-existant folders;
+	-- now they send back null instead of crashing with an unhandled error
+	-- other errors not yet handled
+
+V2.4 2021-01-10: The output of this function should really be an array of file and folder objects rather than generic 'targets'
+	!! Easy scrubing of path names ( cut of bases for security reasons )
+	-- Overall much cleaner code and less uneeded complexity
+	-- Now excludes the original source folder because thats uneeded
+	-- Only files include sizes and modified values
+
 */
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-async function AsyncFileScanRecursive( target = __dirname , opts = {} , parentFolder = '/'  ){
-	async function skipCheck(folderName, parent ){
+let AsyncFileScanRecursive = async( target = __dirname, trunc = /^\//  ) =>{
+
+	let checkItem = ( truePath ) => {
+		const isTrunc = truePath === trunc
+		const relativePath = isTrunc? '/' : truePath.replace(trunc, '')
+		
 		return new Promise( (resolve) => {
-			let skipThis = false;
-			if( opts && opts.skipFolders && opts.skipFolders.length > 0){
-				opts.skipFolders.forEach( bannedFolder => {
-					skipThis = folderName.startsWith(bannedFolder)? true : skipThis
-				})
-			}
-			resolve( skipThis? [] : scanFolder(folderName, parent) );
-		})
-	}
-	
-	async function checkType( item, parent ){
-		return new Promise( (resolve,reject) => {
-			fs.stat(item, (err,stat)=>{
-				if(err){
-					reject(err);
-				}else if( stat.isFile() ){
-					let modified = new Date(stat.mtime)
-					resolve(
-						{ 
-							parent, 
-							item, 
-							type: 'file', 
-							modified,
-							size: stat.size,
-							//stat,	//un-comment if you want to see what other info is available
-							ext: path.extname(item)
-						});					
-				} else {
-				resolve( skipCheck(item,parent) );
+			
+			fs.stat( truePath, (err,stat)=>{
+				if(err ){ 
+					resolve([{err: err.code,attempted:relativePath}])
+						// Put in array incase error thrown by a folder
+				} else { 
+					const { size, mtime }  = stat
+					const isFile = stat.isFile()
+					const modified = new Date(mtime)
+					const ext = isFile? path.extname(relativePath): null
+					const folderItem = ( isTrunc || isFile )? 
+						null: { folder: relativePath }
+					
+					resolve( 
+						stat.isFile()? 
+							{ file: relativePath, modified, size, ext }
+							: scanFolder(truePath, folderItem ) 
+					)
 				}
 			})
 		})
 	}
 	
-	async function scanFolder( folder,parent ){
+	let scanFolder = async( folderPath, subFolderObj )=> {
 		return new Promise( (resolve,reject) => {
-			fs.readdir( folder, {withFileTypes:false}, (err,list) =>{
+			fs.readdir( folderPath, {withFileTypes:false}, (err,list) =>{
 				if(err){
-					return(err);
-				}else {
+					return(err)
+				}else {				
 					let scan = list
-						.map( el => checkType( path.join(folder,el) , folder ) );
-					resolve( Promise.all( [ {item: folder, parent, type:' folder' } ].concat(scan) ) );
+						.map( item => checkItem( `${folderPath}/${item}` ) )
+										
+					resolve( Promise.all( 
+						subFolderObj? [subFolderObj].concat(scan) : scan )	
+					) 
+					// Add subFolderObj or it won't be included in results
 				}
 			})
 		})
 	}
 
-	return await checkType(target, parentFolder );
+	let output = await checkItem(target, trunc )
+	return output? output.flat(Infinity) : null // send null if folder doesnt exist
 }
 
-module.exports = { AsyncFileScanRecursive, test:'test'};
+
+module.exports = { AsyncFileScanRecursive }
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # AsyncFileScanRecursive
 Asynchronously Scan for Files in a Directory and Sub-Directories, Return file/folder names and details
-Allows for ignoring of unwanted directories.
-This Code is very Light Weight at just 2Kb and is designed for use in Node.Js 
-The purpose of this code is to allow the server to locate a list of available files which can then be used as a response to a fetch request. The main use case would be if your users are storing project data in files on your enviornment an need to be able to locate their data. 
+--Allows for ignoring of unwanted directories.--
+	--- That functionality was depricated because this tool is designed for more targeted scanning. ---
+	--- If you have to 'ignore' a folder, then you run the risk of forgetting to ignore it when you really needed to. ---
+
+This Code is very Light Weight at just 3Kb and is designed for use in Node.Js 
+The purpose of this code is to allow the server to locate a list of available files which can then be used as a response to a fetch request. The main use case would be if your users are storing project data in files on your enviornment an need to be able to locate their data.
+
+Updated on 2021-01-26: Now the results array can include Files, Folders, and Errors instead of generic "Items"
+	+ Inaccessible files/folders are returned as an error objects rather than throwing an error which, if unhandled, can mess with your node.js server  
